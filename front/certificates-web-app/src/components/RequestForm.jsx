b8f8b9dd2f94c1735ce10f095c46086e0bb12b49
mrmijan <mijanovic.boki.bojan@gmail.com>
2023-04-18 14:13:02 +0200

    <front> Structured code for frontend but it doesn't work

--- front/certificates-web-app/src/components/RequestForm.jsx
+++ front/certificates-web-app/src/components/RequestForm.jsx
@@ -5,8 +5,10 @@ import {useNavigate} from "react-router-dom";
 
 export default function RequestForm() {
     const [serialNumber, setSerialNumber] = useState("")
+    const [certificateType, setCertificateType] = useState("ROOT")
+    const [endDate, setEndDate] = useState("2000-01-01T00:00:00.000000Z")
+    const [hashAlgorithm, setHashAlgorithm] = useState("")
     const [flags, setFlags] = useState([])
-    console.log(flags)
     //staviti za svako polje
 
     const queryClient = useQueryClient()
@@ -15,17 +17,27 @@ export default function RequestForm() {
     function submitHandler(event) {
         event.preventDefault()
         console.log(flags.sort().join(','))
+        console.log(serialNumber)
+        console.log(endDate)
+        console.log(hashAlgorithm)
+        console.log(certificateType.toUpperCase())
         requestCertificateMutation.mutate()
     }
 
     const requestCertificateMutation = useMutation(() => {
-        return fetch("url apija", {
+        return fetch("https://localhost:7018/api/CertificateRequest/", {
             method: "POST",
             headers: {
                 "content-type": "application/json"
             },
             body: JSON.stringify({
                 parentSerialNumber: serialNumber,
+                o: "test",
+                ou: "string",
+                c: "string",
+                endDate: endDate,
+                type: certificateType,
+                hashAlgorithm: hashAlgorithm,
                 flags: flags.sort().join(',')
             })
         })
@@ -44,15 +56,21 @@ export default function RequestForm() {
             <form onSubmit={submitHandler}>
                 <div>
                     <input type="text" name="parentSerialNumber" onChange={(e) => {setSerialNumber(e.target.value)}}/>
-                    <select>
+                    <select onChange={(e) => {setCertificateType(e.target.value)}}>
                         <option>Root</option>
                         <option>Intermediate</option>
                         <option>End</option>
                     </select>
                 </div>
                 <div>
-                    <input/>
-                    <select></select>
+                    <input type="datetime-local" onChange={(e) => setEndDate(e.target.value)}/>
+                    <select onChange={(e) => setHashAlgorithm(e.target.value)}>
+                        <option>MD5</option>
+                        <option>SHA1</option>
+                        <option>SHA256</option>
+                        <option>SHA384</option>
+                        <option>SHA512</option>
+                    </select>
                 </div>
                 <div>
                     <input value="0" onChange={(event) => {if(event.target.checked) setFlags(prevFlags => [...prevFlags, event.target.value]); else setFlags(prevFlags => prevFlags.filter(flag => flag !== event.target.value))}} type="checkbox"/>
