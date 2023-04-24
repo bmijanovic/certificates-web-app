10d4e1e0ce24f341ece06809de8c6f1d338bff40
mrmijan <mijanovic.boki.bojan@gmail.com>
2023-04-24 17:25:54 +0200

    <front> Initial cards rendering implemented

--- front/certificates-web-app/src/components/CertificateRequestCard.jsx
+++ front/certificates-web-app/src/components/CertificateRequestCard.jsx
@@ -1,26 +1,35 @@
 import React from "react";
 import {useQuery} from "@tanstack/react-query";
+import {Card, CardContent, Grid, Typography} from "@mui/material";
 
-export default function CertificateRequestCard({item}) {
+export default function CertificateRequestCard({data}) {
+    const { id, type, state, hashAlgorithm, endDate, flags, parentSerialNumber } = data;
 
     return <>
-        <div>
-            <div>
-                <img/>
-                <h4>{item.name}</h4>
-                <p>Root</p>
-            </div>
-            <hr/>
-            <p>datum</p>
-            <div>
-                <h5>Serial Number</h5>
-                <h3>3789128931283</h3>
-                <button>View Details</button>
-                <div>
-                    <button>Accept</button>
-                    <button>Decline</button>
-                </div>
-            </div>
-        </div>
+        <Grid item xs={12} sm={6} md={4}>
+            <Card>
+                <CardContent style={{textAlign:"left"}}>
+                    <Typography variant="h5" component="h2">
+                        {parentSerialNumber === "" ? "-" : parentSerialNumber}
+
+                    </Typography>
+                    <Typography color="textSecondary" gutterBottom>
+                        Type: {type}
+                    </Typography>
+                    <Typography color="textSecondary" gutterBottom>
+                        State: {state}
+                    </Typography>
+                    <Typography color="textSecondary" gutterBottom>
+                        Hash Algorithm: {hashAlgorithm}
+                    </Typography>
+                    <Typography color="textSecondary" gutterBottom>
+                        End Date: {endDate}
+                    </Typography>
+                    <Typography color="textSecondary" gutterBottom>
+                        Flags: {flags}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </Grid>
     </>
 }
--- front/certificates-web-app/src/pages/AllCertificateRequests.jsx
+++ front/certificates-web-app/src/pages/AllCertificateRequests.jsx
@@ -1,22 +1,33 @@
 import React from "react";
 import {useQuery} from "@tanstack/react-query";
 import CertificateRequestCard from "../components/CertificateRequestCard.jsx";
+import axios from "axios";
+import {Grid} from "@mui/material";
 
 
 export default function AllCertificateRequests() {
     //https://localhost:7018/api/CertificateRequest
+    // const certificateRequestsQuery = useQuery({
+    //     queryKey: ["certificateRequest"],
+    //     queryFn: () => fetch("https://localhost:7018/api/CertificateRequest").then(res => res.json())
+    // })
+
     const certificateRequestsQuery = useQuery({
         queryKey: ["certificateRequest"],
-        queryFn: () => fetch("https://localhost:7018/api/CertificateRequest").then(res => res.json())
+        queryFn: () => axios.get("https://localhost:7018/api/CertificateRequest").then(res => res.data).catch(err => {console.log(err)})
     })
 
 
 
     return <>
-        <h1>Certificate Requests</h1>
-        {certificateRequestsQuery.isLoading ? <p>Loading...</p> : <div>
-            {certificateRequestsQuery.data.map(item => <CertificateRequestCard key={item.id} item={item}/>)}
+        <div style={{textAlign: "center"}}>
+            <h1>Certificate Requests</h1>
+            {certificateRequestsQuery.isLoading ? <p>Loading...</p> : <div>
+                <Grid container sx={{bx:3}} spacing={5}>
+                    {certificateRequestsQuery.data.map(item => <CertificateRequestCard key={item.id} data={item}/>)}
+                </Grid>
+            </div>
+            }
         </div>
-        }
     </>
 }
